00f00980b8e43479f0a8067d29748f0d677cb53a
denisignatjevs@live.com <Partylifen88>
2019-06-11 23:36:25 +0100

    lesson 3

--- app/src/main/java/com/example/android/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/android/swoosh/WelcomeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.android.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        // так делается кнопка с переходом на другой layout
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java) // explicit intent(we go from one
+            // activity to another. implicit intent - you don't know witch activity you will use, explicit intent -
+            // you know witch activity you will use
+            startActivity(leagueIntent)
+        }
     }
 }
