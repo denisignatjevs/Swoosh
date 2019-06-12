af249f0d546c5e5179d0484ba129bb42064445d0
denisignatjevs@live.com <Partylifen88>
2019-06-12 17:50:11 +0100

    lesson 6

--- app/src/main/java/com/example/android/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/android/swoosh/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.example.android.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
+    // all Kotlin Classes final by default
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    // так делается переход на другую activity(layout)
+    fun leagueNextClicked(view: View) {
+        val leagueIntent = Intent(this, SkillActivity::class.java)
+        startActivity(leagueIntent)
     }
 }
